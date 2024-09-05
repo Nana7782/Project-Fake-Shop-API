76d59400f9879a4e413549680f7aa77bab910b26
Nana Leven <nana.leven@mail.de>
2024-09-05 10:17:25 +0200

    html

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,12 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import { IProdukts, IRating } from "./interfaces/IProducts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const input = document.getElementById("search-input") as HTMLInputElement;
+const sortBy = document.getElementById("sort") as HTMLSelectElement;
+const electroBtn = document.getElementById("electro") as HTMLButtonElement;
+const jeweleryBtn = document.getElementById("jewelery") as HTMLButtonElement;
+const mensBtn = document.getElementById("mens") as HTMLButtonElement;
+const womensBtn = document.getElementById("womens") as HTMLButtonElement;
+const output = document.getElementById("output-container") as HTMLDivElement;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const BASE_URL = "https://fakestoreapi.com/products";
